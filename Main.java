49592f7ac8b15d6feb242da6972693eef1c2b0cd
Kamil Kunce <kunce.kamil@gmail.com>
2023-04-04 23:58:49 +0200

    add check divide by 3,5,15

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\n" + "Hello in FizzBuzz game." + "\n" +
+                "This program check numbers that divide by 3,5,15 beetwen Your first and second number." + "\n" +
+                "Numbers must be integer." + "\n" + "\n"  );
+
+
+
+        System.out.println("Enter the first number" + "\n" + "> ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the second number" + "\n" + "> ");
+        int secondNumber = scanner.nextInt();
+
+        if (firstNumber > secondNumber) {
+            System.out.println("the first number is greater than the second - error");
+        }
+
+        for (int i = firstNumber; i <= secondNumber; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+
+            } else {
+                System.out.println(i);
+            }
+
+        }
     }
 }
